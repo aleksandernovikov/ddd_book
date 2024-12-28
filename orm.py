8e43e6cc218e0df7d98978c580ae90f3649bad15
user <79fz270704@gmail.com>
2024-12-28 18:07:44 +0300

    sqlalchemy 2+ changes

--- orm.py
+++ orm.py
@@ -1,9 +1,8 @@
 from sqlalchemy import Table, MetaData, Column, Integer, String, Date, ForeignKey
-from sqlalchemy.orm import mapper, relationship
-
+from sqlalchemy.orm import relationship, registry
 import model
 
-
+mapper_registry = registry()
 metadata = MetaData()
 
 order_lines = Table(
@@ -35,13 +34,15 @@ allocations = Table(
 
 
 def start_mappers():
-    lines_mapper = mapper(model.OrderLine, order_lines)
-    mapper(
+    mapper_registry.map_imperatively(model.OrderLine, order_lines)
+    mapper_registry.map_imperatively(
         model.Batch,
         batches,
         properties={
             "_allocations": relationship(
-                lines_mapper, secondary=allocations, collection_class=set,
+                model.OrderLine,
+                secondary=allocations,
+                collection_class=set,
             )
         },
     )
